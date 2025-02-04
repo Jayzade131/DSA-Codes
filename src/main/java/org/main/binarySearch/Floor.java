adb500f46ff86fff39f99cc7044986f672cf29ac
jayzade <jay.zade@labhi.in>
2025-02-04 17:01:47 +0530

    code added

---
+++ src/main/java/org/main/binarySearch/Floor.java
@@ -0,0 +1,32 @@
+package org.main.binarySearch;
+
+public class Floor {
+    // equals and smaller than target but greatest number  e target s  , so e answer
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 4, 5, 6, 7, 8, 9};
+        System.out.println(binarySearch(8, arr));
+    }
+
+    public static int binarySearch(int target, int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return end;
+    }
+
+}
