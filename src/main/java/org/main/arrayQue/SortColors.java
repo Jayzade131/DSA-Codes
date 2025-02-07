688432ac283c59949c15a062617ab841c7297894
jayzade <jay.zade@labhi.in>
2025-02-07 15:44:19 +0530

    code added

--- src/main/java/org/main/arrayQue/SortColors.java
+++ src/main/java/org/main/arrayQue/SortColors.java
@@ -1,9 +1,12 @@
 package org.main.arrayQue;
 
+import java.util.Arrays;
+
 public class SortColors {
     public static void main(String[] args) {
         int[] nums = {2, 0, 2, 1, 1, 0};
         sortColors(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
     public static void sortColors(int[] nums) {
